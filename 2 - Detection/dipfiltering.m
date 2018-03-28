7294e4f0d64ad15989ea474494aa1cea5da7d8d6
dtovbis <36423377+dtovbis@users.noreply.github.com>
2018-03-28 12:46:04 -0400

    Update dipfiltering.m

--- 2 - Detection/dipfiltering.m
+++ 2 - Detection/dipfiltering.m
@@ -1,28 +1,62 @@
-% Get list of all TIF files in working directory
+% Get list of all TIF files in working directory and store in a structure
 imagefiles = dir('*.tif');      
 num_images = length(imagefiles);    % Number of files founds
-grayimages=newimar(num_images);
+A=newimar(num_images); %Initializes array of images
+%grayimages=newimar(num_images); %This creates an array of gray images if
+%the original imageset is is colour
 
 for ii=1:num_images
-   currentfilename = imagefiles(ii).name;
-   a = readim(currentfilename);
-   grayimages{ii} = a{2};
+   currentfilename = imagefiles(ii).name; %Extracts the filename
+   A{ii} = readim(currentfilename); %Loads in the image with the given filename to array "A"
+   %grayimages{ii} = a{2}; %This would extract the green layer of the
+   %images if the original imageset is in colour
 end
+%Initializes arrays
 C=newimar(num_images);
 D=newimar(num_images);
 E=newimar(num_images);
 F=newimar(num_images);
-for i=1:num_images
-    C{i}=tophat(grayimages{i},12,'elliptic');
-    D{i}=stretch(C{i},1,99,0,255);
-    E{i}=threshold(D{i},'otsu');
-    F{i}=opening(E{i},7,'elliptic');
+Fsmall=newimar(num_images);
+points=cell(1,num_images);
+
+for i=1:num_images %This loop conducts the filtering
+    C{i}=tophat(A{i},9,'elliptic'); %Removes low intensity background signal for objects greater than given size
+    D{i}=stretch(C{i},1,99,0,255); %Adjusts the histogram to increase contrast
+    E{i}=threshold(D{i},'otsu'); %Automatically thresholds remaining image
+    F{i}=opening(E{i},3,'elliptic'); %Removes objects less than given size
+end
+
+for j=1:num_images
+    msr=measure(F{j},[],{'Size','Perimeter','P2A','Minimum','Maximum'},[]); %Extracts image information for each object in image
+    lmsr=length(msr.size); %Measures the amount of objects detected
+    %for i=1:lmsr ; %The next four lines provide an alternate measure of
+    %"circularity", which dipimages measures through P2A on line 28. I
+    %decided not to use it but I kept the code here just in case
+    %circ(i)=(4*pi*msr.size(i))/(msr.perimeter(i))^2;
+    %end
+    %msr.circularity=circ;
+    ind=find(msr.size>50); %Finds indices of objects greater than size 50 pixels
+    msr_big=msr(ind); %Creates a new measurement array containing only the big objects
+    numbig=size(msr_big.ID,2); %Counts the number of big objects
+    Fsmall{j}=F{j}; %Initializes a new array that will only contain small objects
+        for k=1:numbig %Sets the pixels containing the big objects to 0
+            Fsmall{j}(msr_big.Minimum(1,k):msr_big.Maximum(1,k),msr_big.Minimum(2,k):msr_big.Maximum(2,k))=0;
+        end
+        
+    msrFsmall{j}=measure(Fsmall{j},[],{'Minimum','Maximum'},[]); %Measures properties of the remaining small objects
+    numsmall=size(msrFsmall{j}.ID,2); %Counts the number of small objects
+    
+        for h=1:numsmall %Stores the centroids of all small objects. NOTE that this assumes that all the objects are convex (in our case, they are)
+            points{1,j}(:,1)=msrFsmall{j}.ID;
+            points{1,j}(h,2)=(msrFsmall{j}.Minimum(1,h)+msrFsmall{j}.Maximum(1,h))*0.5;
+            points{1,j}(h,3)=(msrFsmall{j}.Minimum(2,h)+msrFsmall{j}.Maximum(2,h))*0.5;
+        end
 end
 %Plays the series of images defined by the variable in imshow
-for j=1:num_images
-imshow(dip_array(F{j}),[]);
-pause(0.5)
-end
+% for j=1:num_images
+% imshow(dip_array(Fsmall{j}),[]);
+% pause(0.5)
+% end
 %% OLD CODE.
 %From here you can filter it with whatever you want. For example:
 %%Top Hat Filtering
